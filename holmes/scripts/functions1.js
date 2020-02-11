d76ab34f45f134ff86a1473ed85c5db4a6440efd
Jon Holmes <holmjona@isu.edu>
2020-02-10 18:18:52 -0700

    Discussed functions, functial overloading, calling functions. HTML Buttons, and basics of Literal Objects.

---
+++ holmes/scripts/functions1.js
@@ -0,0 +1,44 @@
+﻿
+function one(val) {
+    alert(val);
+}
+
+function one() {
+    alert("Sanjeev");
+}
+
+//one("Dustin","Saurav" , "Saurav","Sanjeev");
+
+function one() {
+    //alert("Saurav" + "Saurav");
+    //alert(arguments[0] + arguments[4] + arguments[3]);
+    //alert(arguments);
+    alert(arguments[0] + arguments[2] + arguments[3]);
+
+}
+
+var happy = function () { alert("Happy"); };
+
+function boo() {
+    alert("Scooby");
+}
+
+
+//boo();
+//happy();
+
+
+var color = function () { return "blue"; };
+function shape() {
+    var retStr = "";
+    for (var i = 0; i < 5; i++) {
+        retStr += i;
+    }
+    return "square" + retStr;
+}
+
+var shape2 = shape;
+
+alert(color());
+alert(shape());
+alert(shape2);
---
+++ holmes/scripts/objects1.js
@@ -0,0 +1,15 @@
+﻿
+var stud1 = { name : "Taylor", siblings : 3};
+
+stud1.pets = 56;
+stud1["lastName"] = "Awesome";
+
+
+function showMeSomething() {
+    alert(stud1.name + " " +
+        stud1.lastName + 
+        " has "
+        + stud1.pets + " pets "
+        + " and has " + stud1.siblings +
+        " siblings.");
+}
