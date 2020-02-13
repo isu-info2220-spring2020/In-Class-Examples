d934a8ec5c0db06541caa670d822fde34575c8e0
Jon Holmes <holmjona@isu.edu>
2020-02-12 18:37:13 -0700

    Talked about prototyping and functional objects. Including the this keyword and functions as properties.

--- holmes/scripts/objects1.js
+++ holmes/scripts/objects1.js
@@ -4,12 +4,34 @@ var stud1 = { name : "Taylor", siblings : 3};
 stud1.pets = 56;
 stud1["lastName"] = "Awesome";
 
+var stud2 = Object.create(stud1);
+
 
 function showMeSomething() {
-    alert(stud1.name + " " +
-        stud1.lastName + 
-        " has "
-        + stud1.pets + " pets "
-        + " and has " + stud1.siblings +
-        " siblings.");
+    //stud2.pets = "twelve";
+    //stud2.__proto__.movie = "Emperor's New Groove";
+    ////alert(stud1.name + " | " + stud2.name);
+
+    //alert(stud1.movie);
+
+    var stud3 = Object.create(stud1);
+    var stud4 = Object.create(stud3);
+    var stud5 = Object.create(stud2);
+
+    stud1.lastName = "Smith";
+    stud2.lastName = "James";
+    var arr = [stud1.lastName, stud2.lastName, stud3.lastName,
+        stud4.lastName, stud5.lastName];
+    alert(arr);
+
+
+    //alert(stud1.name + " " +
+    //    stud1.lastName + 
+    //    " has "
+    //    + stud1.pets + " pets "
+    //    + " and has " + stud1.siblings +
+    //    " siblings.");
 }
+
+
+
---
+++ holmes/scripts/objects2.js
@@ -0,0 +1,43 @@
+﻿//class Person()
+function Person(){ // this is functional object
+    this.firstName = "Bob";
+    var lastName = "Awesome";
+    //alert(lastName);
+}
+
+function thisIsMagic() { // this is function
+    var myNum = 12;
+    myOther = 234;
+
+}
+
+function Pet(name, type) {
+    this.name = name;
+    this.typeOfAnimal = type;
+    this.out = outside;
+    this.makeNoise = makeMoreNoise;
+    this.makeSound = function () {
+        return "Bark";
+    };
+
+
+    function makeMoreNoise() {
+        return "Howwwwl";
+    }
+
+}
+
+function outside() {
+    return "out";
+    
+}
+
+
+function somethingElse() {
+
+    var bob = new Person();
+    var pet = new Pet("Diesel","Dog");
+    alert(bob.firstName);
+
+}
+
