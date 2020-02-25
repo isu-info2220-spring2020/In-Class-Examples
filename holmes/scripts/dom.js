e4c537646b4204611521e13c0f081fd59d80e88e
Jon Holmes <holmjona@isu.edu>
2020-02-25 00:05:19 -0700

    Discussed more about DOM and started events discusssion.

--- holmes/scripts/dom.js
+++ holmes/scripts/dom.js
@@ -18,29 +18,55 @@ var sec = document.getElementById("moreContent");
 //    alert(lis[ndx].outerHTML);
 //}
 
-var newTextNode = document.createTextNode("new Text");
-//alert(newTextNode);
-hypGo.appendChild(newTextNode);
+//var newTextNode = document.createTextNode("new Text");
+////alert(newTextNode);
+//hypGo.appendChild(newTextNode);
 
-var liPar = hypGo.parentNode.parentNode;
+//var liPar = hypGo.parentNode.parentNode;
 
 
-var newNode = document.createElement("li");
-//newNode.innerText = "New Node";
-newNode.appendChild(newTextNode);
-//alert(newNode);
-liPar.appendChild(newNode);
+//var newNode = document.createElement("li");
+////newNode.innerText = "New Node";
+//newNode.appendChild(newTextNode);
+////alert(newNode);
+//liPar.appendChild(newNode);
 
-function button_Click() {
-    var newText = document.createTextNode("New Text");
-    var thisButton = document.getElementById("myButton");
-    thisButton.appendChild(newText);
-    thisButton.setAttribute("class", "hoopy");
-    thisButton.className = "nothing";
-}
+//function button_Click() {
+//    var newText = document.createTextNode("New Text");
+//    var thisButton = document.getElementById("myButton");
+//    thisButton.appendChild(newText);
+//    thisButton.setAttribute("class", "hoopy");
+//    thisButton.className = "nothing";
+//}
 
-var firstChild = liPar.childNodes[0]; // UL -> first child.
-alert(firstChild);
-alert(firstChild.nextSibling);
-alert(firstChild.nextSibling.nextSibling);
-alert(firstChild.nextSibling.nextSibling.nextSibling);
+//var firstChild = liPar.childNodes[0]; // UL -> first child.
+//alert(firstChild);
+//alert(firstChild.nextSibling);
+//alert(firstChild.nextSibling.nextSibling);
+//alert(firstChild.nextSibling.nextSibling.nextSibling);
+
+// ALL LIs on the page (in the document)
+//var lis = document.getElementsByTagName("li");
+
+//for (var i = 0; i < lis.length; i++) {
+//    alert(lis[i].innerText);
+//}
+
+
+//var ols = document.getElementsByTagName("ol");
+
+////for (var i = 0; i < ols.length; i++) {
+////    alert(ols[i].innerHTML);
+////}
+
+//var myOL = ols[0];
+//var lis = myOL.getElementsByTagName("li");
+
+//for (var i = 0; i < lis.length; i++) {
+//    alert(lis[i].innerText);
+//}
+
+//var frm = document.getElementById("contact");
+//var div = document.getElementById("contact-form");
+////var o = div.getElementById("contact");
+//alert(o);
---
+++ holmes/scripts/events-basic.js
@@ -0,0 +1,46 @@
+﻿// fires when the widnow loads.
+//window.onload = function () {
+//    alert("Window Loaded");
+//};
+
+window.onload = windowLoad;
+
+function windowLoad() {
+    //alert("Window still loaded");
+
+    //var ani = new Animal("fido");
+    //ani.name = "fluffy";
+
+    var btn = document.getElementById("myButton");
+    //alert(btn);
+    btn.onclick = function () {
+        alert("in JS");
+    };
+    btn.addEventListener("click", function () {
+        alert("in JS 2");
+    });
+    btn.addEventListener("click", btn_click);
+    btn.onclick = function () {
+        alert("in JS 4");
+    };
+
+    btn.onmouseenter = mouseHere;
+
+}
+
+function btn_click() {
+    alert("in JS 3");
+}
+
+function mouseHere(e) {
+    e = e || window.event;
+    alert(e.x + " | " + e.y);
+}
+
+
+////alert("did I load");
+//var btn = document.getElementById("myButton");
+////alert(btn);
+//btn.onclick = function () {
+//    alert("in JS");
+//};
