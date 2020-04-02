2ce0a8d514593c112d98f243290238cda93ad813
Jon Holmes <holmjona@isu.edu>
2020-04-02 07:36:36 -0600

    added and discussed jQuery UI.

---
+++ holmes/scripts/use-jqueryui.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#accordion").accordion();
+    $(".draggable").draggable();
+    $("#sort-me").addClass("ui-selectable")
+        .sortable();
+    var students = [
+        "Porter",
+        "Dustin",
+        "James",
+        "Taylor",
+        "Saurav",
+        "Saurav",
+        "Braeden",
+        "Sanjeev"
+    ];
+
+    $("#autocomplete").autocomplete({
+        source: students
+    });
+
+    $("button").button();
+
+    $("#radioset").buttonset();
+    $(".datepickable").datepicker();
+
+});
