ed35e80c99f35799f631ab91f22acdbeb4078efc
Jon Holmes <holmjona@isu.edu>
2020-04-13 18:16:17 -0600

    Talked about cookies and started talking about local storage.

---
+++ holmes/scripts/cookies.js
@@ -0,0 +1,34 @@
+﻿window.onload = function () {
+    var btn = this.document.getElementById("btnAddCookie");
+    var btn2 = this.document.getElementById("btnAddCookie2");
+    var btnr = this.document.getElementById("btnReadCookie");
+
+    btn.onclick = function () {
+        document.cookie = "type=snicker doodle";
+        document.cookie = "bigthing=adlskfjaweljfalsdijfoaisefoawhefoiasefilawjeilfaweoifheilfhaeilfasliefweoilfaseiohfaweoifaeiljfaseiljfoaseiljfaweiljfaweoijfaeoijfeiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaiiiiiiiiiiiiiiiiiiiiiiiiiiiiii44444444444444444444444444444"
+    };
+
+    btn2.onclick = function () {
+        document.cookie = "type=macadamian chip";
+        document.cookie = "drink=milk";
+    };
+
+    btnr.onclick = function () {
+        alert(document.cookie);
+    };
+
+    this.document.getElementById("btnSaveName").onclick = function () {
+        txt = document.getElementById("txtName");
+        document.cookie = "name=" + txt.value;
+    };
+
+    var cooks = this.document.cookie;
+    var cooksplit = cooks.split(";");
+    for (var i = 0; i < cooksplit.length; i++) {
+        cookparts = cooksplit[i].split("=");
+        if (cookparts[0].trim() === "name") {
+            alert("Welcome back " + cookparts[1]);
+        }
+    }
+
+};
---
+++ holmes/scripts/storage-basics.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    //var btnSave = this.document.getElementById("btnSave");
+
+    if (Storage) {
+        // we have storage
+        this.localStorage.setItem("name", "Bob");
+        this.localStorage["age"] = 89;
+        this.localStorage.color = "Red";
+
+        alert(this.localStorage.name);
+        alert(this.localStorage["color"]);
+        alert(this.localStorage.getItem("age"));
+
+
+    } else {
+        //Oops no storage
+    }
+
+};
