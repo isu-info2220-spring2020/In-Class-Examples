1e4eb727028e8f43d50cf22b4885537c428afa26
Jon Holmes <holmjona@isu.edu>
2020-04-27 18:18:48 -0600

    Talked about AJAX and JSON again.

---
+++ holmes/scripts/dogs.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function(){
+    $("#btnLoadDogs").click(function () {
+        $.ajax({
+            url: "data/dogs.json"
+            , method: "get"
+            , success: function (data) {
+                for (var ndx = 0; ndx < data.length; ndx++) {
+                    var dog = data[ndx];
+                    addDog(dog.name, dog.breed);
+                    addDogNative(dog.name, dog.breed);
+                }
+            }
+            , error: function () {
+                addDog("Error");        
+            }
+        });
+    });
+});
+
+function addDog(name) {
+    var li = $("<li>");
+    li.text(name);
+    var spn = $("<span>");
+    spn.text(arguments[1] + ": ");
+    li.prepend(spn);
+    $("#ulDogs").append(li);
+}
+
+function addDogNative(name) {
+    var li = document.createElement("li");
+    var spn = document.createElement("span");
+    spn.innerText = arguments[1] + ": ";
+    //var nameNode = document.createTextNode(name);
+    //li.appendChild(spn);
+    //li.appendChild(nameNode);
+    li.appendChild(spn);
+    alert(li.innerHTML);
+    name = "<p>" + name + "</p>";
+    li.innerText += name;
+    alert(li.innerHTML);
+    var ul = document.getElementById("ulDogs");
+    ul.appendChild(li);
+}
