17e59d6a6679bbcba8bb172e6c8ca4ad6f36dfa2
Jon Holmes <holmjona@isu.edu>
2020-04-29 18:10:00 -0600

    Covered Functional Vs Literal Objects

---
+++ holmes/scripts/review.js
@@ -0,0 +1,10 @@
+﻿function Student(name,age) {
+    this.name = name;
+    this.age = age;
+}
+
+var litObj = { name: "Frank", age: 45 };
+var funObj = new Student("Sally", 25);
+
+alert(litObj.name);
+alert(funObj.name);
